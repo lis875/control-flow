3d84df1c5005c9b7fb8bbc3870edcf8f78ca9121
Rick Wash <wash@msu.edu>
2019-08-12 14:42:56 -0700

    Initial mad-libs example

---
+++ README.md
@@ -0,0 +1,26 @@
+To run this program, use
+```
+python mad_libs.py
+```
+
+Here is some example output:
+```
+Name of a place: Ann Arbor
+An action that someone an do: sleep
+Another action: jump
+Thing that makes noise: whoopie cushion
+Something that you can buy: shoes
+How tall are you? six foot
+Something you an hold in your hands: phone
+
+
+Do you come from Ann Arbor?
+Where women sleep and men jump?
+Can't you hear, can't you hear the whoopie cushion?
+You better run, you better take cover
+
+Buying shoes from a man in Brussels
+He was six foot and full of muscles
+I said, "do you speak-a my language?"
+He just smiled and gave me a phone
+```
---
+++ mad-libs.py
@@ -0,0 +1,24 @@
+# Ask the user for some words
+place = raw_input("Name of a place: ")
+action = raw_input("An action that someone an do: " )
+action2 = raw_input("Another action: ")
+noisemaker = raw_input("Thing that makes noise: ")
+buy = raw_input("Something that you can buy: ")
+height = raw_input("How tall are you? ")
+gift = raw_input("Something you an hold in your hands: ")
+
+
+# Print out a blank line or two
+print("")
+print("")
+
+# print out the song lyrics to "Down Under" by Men at Work, with the words filled in
+print("Do you come from " + place + "?")
+print("Where women " + action + " and men " + action2 + "?")
+print("Can't you hear, can't you hear the " + noisemaker + "?")
+print("You better run, you better take cover")
+print("")
+print("Buying " + buy + " from a man in Brussels")
+print("He was " + height + " and full of muscles")
+print('I said, "do you speak-a my language?"')
+print("He just smiled and gave me a " + gift)
