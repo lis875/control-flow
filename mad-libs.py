531c33ffdc483d01725ad0e28798f259c16d87f6
Rick Wash <wash@msu.edu>
2019-08-13 10:34:07 -0700

    More mad-libs

--- mad-libs.py
+++ mad-libs.py
@@ -1,24 +1,14 @@
-# Ask the user for some words
-place = raw_input("Name of a place: ")
-action = raw_input("An action that someone an do: " )
-action2 = raw_input("Another action: ")
-noisemaker = raw_input("Thing that makes noise: ")
-buy = raw_input("Something that you can buy: ")
-height = raw_input("How tall are you? ")
-gift = raw_input("Something you an hold in your hands: ")
+# Fill in some words
+body_part = "head"
+action = "catch"
+action_with_ing = "eating"
 
-
-# Print out a blank line or two
-print("")
+# print out the quotes from Guardians of the Galaxy, with the words filled in
+print("Rocket Raccoon: His people are entirely literal. Metaphors go right over his", body_part)
+print("Drax the Destroyer: Nothing goes over my", body_part, ". My reflexes are excellent and I would",  action, "it.")
+ 
 print("")
 
-# print out the song lyrics to "Down Under" by Men at Work, with the words filled in
-print("Do you come from " + place + "?")
-print("Where women " + action + " and men " + action2 + "?")
-print("Can't you hear, can't you hear the " + noisemaker + "?")
-print("You better run, you better take cover")
-print("")
-print("Buying " + buy + " from a man in Brussels")
-print("He was " + height + " and full of muscles")
-print('I said, "do you speak-a my language?"')
-print("He just smiled and gave me a " + gift)
+print("Star-Lord: Oh will you shut up about that? God, 20 years you've been throwing that in my face. Like it's some great thing, \"not", action_with_ing, "me\". Normal people don't even think about",  action_with_ing,  "someone else, much less, that person having to be grateful for it.")
+
+
